057a4a22ca95f4f2f074fb6277eb0d009490751a
bkrall36 <brendonkrall@gmail.com>
2022-09-07 13:21:20 -0500

    Perform restricted DiD analysis
    Perform restricted DiD analysis by dropping districts with planned treatment.

--- Code/elections_data_cleaning.R
+++ Code/elections_data_cleaning.R
@@ -18,23 +18,11 @@ df <- df %>% mutate(
 planned_districts <- c('Islamabad', 'Rawalpindi', 'Sukkur', 'Hyderabad', 'Peshawar', 'Lahore', 'Abbottabad', 'Faisalabad', 'Multan')
 incidental_districts <- c('Moro (Naushehro Feroz)', 'Khairpur', 'Larkana', 'Nawabshah',  'Matiari', 'Attock', 'Gujarat', 'Gujranwala', 'Taxila', 'Haripur', 'Chakwal', 'Pindi Bhattian', 'Chiniot', 'Sahiwal', 'Okara', 'Chichawatni', 'Khanewal')
 
-# Create Incidental Visit Treatment Indicator
-df <- df %>% mutate(
-  incidental_treatment = ifelse((districts %in% incidental_districts), 1, 0),
-  planned_treatment = ifelse((districts %in% planned_districts), 1, 0)
-)
-
-# Create Visits 2008 and 2013 variable Counts
-#df <- df %>% mutate(
-#  treated_2008_n = ifelse((Year == 2008 & Treated == 1), 'Visits before 2008 elections', 0),
-#  treated_2013_n = ifelse((Year == 2013 & Treated == 1), 'N_visits', 0)
-#)
-
 # Remove observations with NA values for our outcome variable 
 df <- df %>% filter(!is.na(Vote_Share))
 
 # Select columns needed for analysis 
-df <- df %>% select(c('Year', 'year_dummy', 'PA ID', 'constituency_dummy', 'districts', 'Party Initials', 'Vote_Share.1', 'count', 'treated', 'planned_treatment', 'incidental_treatment', '1st_visit_year'))
+df <- df %>% select(c('Year', 'year_dummy', 'PA ID', 'constituency_dummy', 'districts', 'Party Initials', 'Vote_Share.1', 'count', 'treated', '1st_visit_year'))
 # Store Clean Data
 write_csv(df, paste0(
   Outputs, "clean_elections_data.csv"))
@@ -54,3 +42,16 @@ df_pml_n <- df_02_08 %>%
 df_pppp <- df_02_08 %>%
   filter(`Party Initials` == 'PPPP')
 
+# Subset Dataset for Regression Analysis with Restricted Sample
+# Restrict analysis to years between 2002 and 2008
+df_02_08_restricted <- subset(df_02_08, !(districts %in% planned_districts))
+
+# Subset data by party affiliation 
+df_pml_restricted <- df_02_08 %>%
+  filter(`Party Initials` == 'PML')
+
+df_pml_n_restricted <- df_02_08 %>%
+  filter(`Party Initials` == 'PML-N')
+
+df_pppp_restricted <- df_02_08 %>%
+  filter(`Party Initials` == 'PPPP')
--- Code/table04_DiDregressionrestricted.R
+++ Code/table04_DiDregressionrestricted.R
@@ -3,33 +3,33 @@ setwd(Outputs)
 
 # Perform Regression Analysis - PML
 # OLS 
-reg_pml <- lm(Vote_Share.1 ~ treated, data = df_pml)
+reg_pml <- lm(Vote_Share.1 ~ treated, data = df_pml_restricted)
 # cluster-robust SEs for reg_pml
 cl.cov.pml1 <- cluster.vcov(reg_pml, df_pml$`PA ID`) 
 cl.robust.se.pml1 <- sqrt(diag(cl.cov.pml1))
 
 # OLS with 'count' variable control
-reg_pml2 <- lm(Vote_Share.1 ~ treated + year_dummy, data = df_pml)
+reg_pml2 <- lm(Vote_Share.1 ~ treated + year_dummy, data = df_pml_restricted)
 # cluster-robust SEs for reg_pml2
 cl.cov.pml2 <- cluster.vcov(reg_pml2, df_pml$`PA ID`) 
 cl.robust.se.pml2 <- sqrt(diag(cl.cov.pml2))
 
 # OLS with TWFE 
-reg_pml3 <- lm(Vote_Share.1 ~ treated + year_dummy + constituency_dummy, data = df_pml)
+reg_pml3 <- lm(Vote_Share.1 ~ treated + year_dummy + constituency_dummy, data = df_pml_restricted)
 # cluster-robust SEs for reg_pml3
 cl.cov.pml3 <- cluster.vcov(reg_pml3, df_pml$`PA ID`) 
 cl.robust.se.pml3 <- sqrt(diag(cl.cov.pml3))
 
 # OLS with TWFE + 'count' control variable 
-reg_pml4 <- lm(Vote_Share.1 ~ treated + year_dummy + constituency_dummy + count, data = df_pml)
+reg_pml4 <- lm(Vote_Share.1 ~ treated + year_dummy + constituency_dummy + count, data = df_pml_restricted)
 # cluster-robust SEs for reg_pml4
 cl.cov.pml4 <- cluster.vcov(reg_pml4, df_pml$`PA ID`) 
 cl.robust.se.pml4 <- sqrt(diag(cl.cov.pml4))
 
-table02_DiDregression <- stargazer(reg_pml,reg_pml2,reg_pml3,reg_pml4, 
+table04_DiDregression <- stargazer(reg_pml,reg_pml2,reg_pml3,reg_pml4, 
                                    type="text",
                                    style = 'aer',
-                                   title="Table 2 - Effect of CJ visits on vote share for General Musharraf PML - Impact on Dicator’s Party", 
+                                   title="Table 2 - Effect of CJ visits on vote share for General Musharraf PML (restricted)", 
                                    keep = c('treated'), 
                                    order=c('treated'), 
                                    covariate.labels=c('CJ Visits'), 
@@ -50,35 +50,35 @@ table02_DiDregression <- stargazer(reg_pml,reg_pml2,reg_pml3,reg_pml4,
 # Perform Regression Analysis for Opposition 
 ## PML-N
 # OLS with TWFE 
-reg_pmln3 <- lm(Vote_Share.1 ~ treated + year_dummy + constituency_dummy, data = df_pml_n)
+reg_pmln3 <- lm(Vote_Share.1 ~ treated + year_dummy + constituency_dummy, data = df_pml_n_restricted)
 # cluster-robust SEs for reg_pmln3
 cl.cov.pmln3 <- cluster.vcov(reg_pmln3, df_pml_n$`PA ID`) 
 cl.robust.se.pmln3 <- sqrt(diag(cl.cov.pmln3))
 
 # OLS with TWFE + 'count' control variable 
-reg_pmln4 <- lm(Vote_Share.1 ~ treated + year_dummy + constituency_dummy + count, data = df_pml_n)
+reg_pmln4 <- lm(Vote_Share.1 ~ treated + year_dummy + constituency_dummy + count, data = df_pml_n_restricted)
 # cluster-robust SEs for reg_pmln4
 cl.cov.pmln4 <- cluster.vcov(reg_pmln4, df_pml_n$`PA ID`) 
 cl.robust.se.pmln4 <- sqrt(diag(cl.cov.pmln4))
 
 ## PPP
 # OLS with TWFE 
-reg_pppp3 <- lm(Vote_Share.1 ~ treated + year_dummy + constituency_dummy, data = df_pppp)
+reg_pppp3 <- lm(Vote_Share.1 ~ treated + year_dummy + constituency_dummy, data = df_pppp_restricted)
 # cluster-robust SEs for reg_pppp3
 cl.cov.pppp3 <- cluster.vcov(reg_pppp3, df_pppp$`PA ID`) 
 cl.robust.se.pppp3 <- sqrt(diag(cl.cov.pppp3))
 
 # OLS with TWFE + 'count' control variable 
-reg_pppp4 <- lm(Vote_Share.1 ~ treated + year_dummy + constituency_dummy + count, data = df_pppp)
+reg_pppp4 <- lm(Vote_Share.1 ~ treated + year_dummy + constituency_dummy + count, data = df_pppp_restricted)
 # cluster-robust SEs for reg_pppp4
 cl.cov.pppp4 <- cluster.vcov(reg_pppp4, df_pppp$`PA ID`) 
 cl.robust.se.pppp4 <- sqrt(diag(cl.cov.pppp4))
 
 # Create Opposition (PML-N and PPP) Regression Table
-table021_DiDregressionOpp <- stargazer(reg_pmln3,reg_pmln4,reg_pppp3,reg_pppp4, 
+table041_DiDregressionOpp <- stargazer(reg_pmln3,reg_pmln4,reg_pppp3,reg_pppp4, 
                                       type="text",
                                       style = 'aer', 
-                                      title="Table 3 - Effect of CJ visits on vote share for the Opposition (PML-N & PPP)", 
+                                      title="Table 3 - Effect of CJ visits on vote share for the Opposition (restricted)", 
                                       keep = c('treated'), 
                                       order=c('treated'), 
                                       covariate.labels=c('CJ Visits'), 
@@ -95,4 +95,3 @@ table021_DiDregressionOpp <- stargazer(reg_pmln3,reg_pmln4,reg_pppp3,reg_pppp4,
                                       keep.stat=c('n', 'adj.rsq', 'f'), 
                                       add.lines = list('Year FE' = c('Year FE','Yes', 'Yes', 'Yes', 'Yes'), 'Constituency FE' = c('Constituency FE','Yes', 'Yes', 'Yes', 'Yes'), 'Controls' = c('Controls','No', 'Yes', 'No', 'Yes'), "Mean" = c("Mean", round(mean(df_pml_n$Vote_Share.1), 2), round(mean(df_pml_n$Vote_Share.1), 2), round(mean(df_pppp$Vote_Share.1), 2), round(mean(df_pppp$Vote_Share.1), 2))),
                                       out="regression_opp_2008DiD.html")
-
